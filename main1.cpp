996cfecd5cf9a22b5c7221820afced77f64da9cd
Avsyankaa <32384402+Avsyankaa@users.noreply.github.com>
2017-11-10 22:15:07 +0300

    Update main1.cpp

--- main1.cpp
+++ main1.cpp
@@ -7,58 +7,33 @@ int main() {
   string array;
   string result;
   string count;
-  string part1;
-  string part2;
-  string integer_str;
+  string part1, part2;
   int length_int;
   int counter = 0;
-  int k = 0;
   int length1 = 0;
-  int length2 = 0;
-  int z = 0;
   cin >> length;
   cin.ignore(numeric_limits<streamsize>::max(), '\n');
   getline(cin, array);
-  cin >> count;
   length_int = atoi(length.c_str());
   for (int i = 0; i < array.length(); i++) {
     if (array[i] == ' ') counter++;
   }
-  if ((length_int != (counter + 1)) || (count == " "))
+  if (length_int != (counter + 1))
     cout << " An error has occurred while reading" << endl;
   else {
-    length1 = atoi(count.c_str()) * 2 - 1;
-    length2 = array.length() - length1;
-    for (int i = 0; i < length1; i++) {
-      part1 = part1 + array[i];
-    }
-    for (int i = part1.length(); i < array.length(); i++) {
-      part2 = part2 + array[i];
-    }
-    for (int i = 0; i < (length1 / 2); i++) {
-      swap(part1[i], part1[length1 - i - 1]);
-    }
-    for (int i = 0; i < (length2 / 2); i++) {
-      swap(part2[i], part2[length2 - i - 1]);
-    }
-    array = part2 + part1;
-    while (true) {
-      for (int i = z; i < array.length(); i++) {
-        if (array[i] == ' ') {
-          z++;
-          break;
-        }
-        integer_str = integer_str + array[i];
-        z++;
+    cin >> count;
+    if (atoi(count.c_str()) == 0) {
+      cout << " An error has occurred while reading" << endl;
+    } else {
+      length1 = (length_int - atoi(count.c_str())) * 2;
+      for (int i = 0; i < length1; i++) {
+        part2 = part2 + array[i];
       }
-      if (integer_str == "") break;
-      cout << integer_str << endl;
-      for (int j = 0; j < ((integer_str.length()) / 2); j++) {
-        swap(integer_str[j], integer_str[integer_str.length() - j - 1]);
+      for (int i = length1; i < array.length(); i++) {
+        part1 = part1 + array[i];
       }
-      result = result + integer_str + " ";
-      integer_str = "";
+      result = part1 + ' ' + part2;
+      cout << result << endl;
     }
-    cout << result << endl;
   }
 }
